2cb6e1189167e0937974a14cfcc56cf2b7329ba3
HOMECHOICE\WESTHS <svdwesthuizen@homechoice.co.za>
2018-06-05 07:13:27 +0200

    Added new components and routing

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +12,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,7 +21,9 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       this.alertify.success('logged in successfully');
     }, error => {
-      this.alertify.error(error);
+      this.alertify.error('Failed to log in.');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -27,6 +31,7 @@ export class NavComponent implements OnInit {
     this.authService.userToken = null;
     localStorage.removeItem('token');
     this.alertify.message('logged out.');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
